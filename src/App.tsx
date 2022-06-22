f359c2c43f9746fd20212d8d1e2b9681c9a63a2b
Ken San Angelo <ken@kensanangelo.com>
2022-06-22 15:04:08 -0400

    Adds react router and changes header links to support it

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import Home from './pages/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-	return <Home />;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Home />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,20 @@
 import './header.scss';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
 	return (
 		<header className='header'>
 			<h1 className='header__title'>Dog App</h1>
 			<nav className='header__nav'>
-				<a className='header__link' href='#'>
+				<Link className='header__link' to='#'>
 					Home
-				</a>
-				<a className='header__link' href='#'>
+				</Link>
+				<Link className='header__link' to='#'>
 					About
-				</a>
-				<a className='header__link' href='#'>
+				</Link>
+				<Link className='header__link' to='#'>
 					Contact
-				</a>
+				</Link>
 			</nav>
 		</header>
 	);
