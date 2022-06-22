dad35f0e1fe7438077fc6aedff982548329afd0c
Ken San Angelo <ken@kensanangelo.com>
2022-06-22 14:27:15 -0400

    Adds loading state to images to reduce reflow

--- src/components/BreedCard/index.tsx
+++ src/components/BreedCard/index.tsx
@@ -1,4 +1,6 @@
 import './breedCard.scss';
+import { useState } from 'react';
+import clsx from 'clsx';
 
 interface Props {
 	name: string;
@@ -6,12 +8,20 @@ interface Props {
 }
 
 const BreedCard = ({ name, imgLink }: Props) => {
+	const [loading, setLoading] = useState(true);
+
 	return (
 		<article className='breed-card'>
+			{loading ? <div className='breed-card__placeholder'></div> : null}
 			<img
-				className='breed-card__img'
+				className={clsx(
+					'breed-card__img',
+					loading && 'breed-card__img--hidden'
+				)}
+				height='200px'
 				src={imgLink}
 				alt={`A photo of a ${name}`}
+				onLoad={() => setLoading(false)}
 			/>
 			<h3 className='breed-card__name'>{name}</h3>
 		</article>
